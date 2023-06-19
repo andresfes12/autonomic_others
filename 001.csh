dd09399e0043900ad58a59808c1ad8016ea0008d
andresfes12 <136843764+andresfes12@users.noreply.github.com>
2023-06-19 14:44:28 -0500

    Update and rename 001.rs to 001.csh

---
+++ 001.csh
@@ -0,0 +1,6 @@
+#!/bin/tcsh
+
+set num = 6
+set num2 = 8
+@ sum = $num + $num2
+echo $sum
--- 001.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let a = 5;
-    let b = 90;
-    let sum = a + b;
-    println!("{}",sum)
-}
