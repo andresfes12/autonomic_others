f7f25e77a40c6ff8933a2b063dafe1726f0219fc
andresfes12 <136843764+andresfes12@users.noreply.github.com>
2023-06-22 18:15:10 -0500

    Update 003.cr

--- 003.cr
+++ 003.cr
@@ -1,13 +1,8 @@
 array1 = [10, 20, 30]
 array2 = [5, 15, 25]
 
-if array1.size != array2.size
-  puts "Las matrices no tienen la misma longitud"
-  exit(1)
-end
-
 length = array1.size
 (0...length).each do |i|
   sum = array1[i] + array2[i]
-  puts "Suma en la posición #{i}: #{sum}"
+  puts #{sum}
 end
