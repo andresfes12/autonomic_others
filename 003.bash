2fe7aaf09a8c8563a7e142d7082f565c50868f3b
andresfes12 <136843764+andresfes12@users.noreply.github.com>
2023-06-22 18:14:21 -0500

    Update 003.bash

--- 003.bash
+++ 003.bash
@@ -4,14 +4,8 @@
 array1=(10 20 30)
 array2=(5 15 25)
 
-if [ ${#array1[@]} -ne ${#array2[@]} ]; then
-    echo "Las matrices no tienen la misma longitud"
-    exit 1
-fi
-
-
 length=${#array1[@]}
 for ((i = 0; i < length; i++)); do
     sum=$((array1[i] + array2[i]))
-    echo "Suma en la posición $i: $sum"
+    echo $sum
 done
