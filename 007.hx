a6c31d74bf0236f0ad7c4011ef4528850ad749e6
andresfes12 <136843764+andresfes12@users.noreply.github.com>
2023-06-29 19:39:18 -0500

    Update 007.hx

--- 007.hx
+++ 007.hx
@@ -1,35 +1,23 @@
 class Main {
-    static function main() {
-        var numTestCases:Int = Std.parseInt(sys.stdin.readLine());
-        var testCases:Array<Array<Int>> = new Array<Array<Int>>();
-
-        for (i in 0...numTestCases) {
-            var line:String = sys.stdin.readLine();
-            var isles:Array<Int> = line.split(" ").map(function(s:String) return Std.parseInt(s));
-            testCases.push(isles);
-        }
-
-        for (testCase in testCases) {
-            var maxCandies:Int = getMaxCandies(testCase, testCase.length);
-            sys.stdout.write(maxCandies.toString());
-
-            if (testCase != testCases[testCases.length - 1]) {
-                sys.stdout.write(" ");
-            }
-        }
-
-        sys.stdout.write("\n");
+    static function fahrenheitToCelsius(fahrenheit: Float): Float {
+        return (fahrenheit - 32) * 5 / 9;
     }
 
-    static function getMaxCandies(isles:Array<Int>, i:Int):Int {
-        if (i <= 0) {
-            return 0;
-        } else if (i == 1) {
-            return isles[0];
-        } else {
-            var maxCandiesWithoutJump:Int = getMaxCandies(isles, i - 1);
-            var maxCandiesWithJump:Int = getMaxCandies(isles, i - 2) + isles[i - 1];
-            return Math.max(maxCandiesWithoutJump, maxCandiesWithJump);
+    static function convertTemperatures(temperatures: Array<Float>): Void {
+        for (temp in temperatures) {
+            var celsius = fahrenheitToCelsius(temp);
+            trace(celsius);
         }
+    }
+
+    static function main() {
+        var numTemps = Std.parseInt(sys.io.readLine());
+        var temperatures = new Array<Float>();
+        for (i in 0...numTemps) {
+            var temp = Std.parseFloat(sys.io.readLine());
+            temperatures.push(temp);
+        }
+        convertTemperatures(temperatures);
     }
 }
+
